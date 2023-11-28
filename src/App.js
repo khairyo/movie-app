9ebfcac00cb035e2715cfb03f0c3f5f4297caa66
khairyo <134465277+khairyo@users.noreply.github.com>
2023-11-28 20:23:14 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import MovieCard from './MovieCard';
 
 const API_URL = 'http://www.omdbapi.com?apikey=8e614931';
 
-const movie1 = {
-  "Title": "Batman v Superman: Dawn of Justice",
-  "Year": "2016",
-  "imdbID": "tt2975590",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-}
-
 function App() {
 
   const [movies, setMovies] = useState([]); 
